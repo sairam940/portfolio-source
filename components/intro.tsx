7283cc801fc3b4522e164cbb1f8a973d8116c3e1
sairam940 <38803518+sairam940@users.noreply.github.com>
2026-01-18 16:29:40 -0600

    renamed resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -90,7 +90,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Ram_Pullabhotla_Sr SW Engineer_Autoamation_Resume.pdf"
+          href="/Ram_Pullabhotla_Sr SW Engineer_Automation_Resume.pdf"
           download
         >
           Download Resume{" "}
